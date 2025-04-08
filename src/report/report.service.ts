647565c56b314e2f9d551a65ca81775aac6fbf8e
chris.wz <scissorwz@gmail.com>
2025-04-07 23:48:57 -0700

    add more service of report

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -9,36 +9,111 @@ export class ReportService {
     }
 
     async findById(id: string) {
-        const file = await this.prisma.file.findUnique({
-            where: { id: id },
-            include: {
-                filetype: true
+        try {
+            const file = await this.prisma.file.findUnique({
+                where: { id: id },
+                include: {
+                    filetype: true
+                }
+            })
+
+            if (file?.filetype.typename === 'Safety Plan') {
+                return await this.prisma.safetyPlan.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
             }
-        })
 
-        if (file?.filetype.typename === 'Safety Plan') {
-            return this.prisma.safetyPlan.findMany({
-                where: {
-                    fileId: id
-                }
-            })
-        }
+            if (file?.filetype.typename === 'Flow Through') {
+                return await this.prisma.flowThrough.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
 
-        if (file?.filetype.typename === 'Flow Through') {
-            return this.prisma.flowThrough.findMany({
-                where: {
-                    fileId: id
-                }
-            })
-        }
+            if (file?.filetype.typename === 'Loss of Service') {
+                return await this.prisma.lossOfService.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
 
-        if (file?.filetype.typename === 'Loss of Service') {
-            return this.prisma.lossOfService.findMany({
-                where: {
-                    fileId: id
-                }
-            })
+            if (file?.filetype.typename === 'Rent Supplement Request') {
+                return await this.prisma.rentSupplementRequest.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Goals and Progress') {
+                return await this.prisma.goalsAndProgress.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Overdose Safety Plan') {
+                return await this.prisma.overdoseSafetyPlan.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Incident Report') {
+                return await this.prisma.incidentReport.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Individuals') {
+                return await this.prisma.individuals.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Shelter Diversion Follow-Up Log') {
+                return await this.prisma.shelterDiversionLog.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Site List') {
+                return await this.prisma.siteList.findMany({
+                    where: {
+                        fileId: id
+                    }
+                })
+            }
+
+            if (file?.filetype.typename === 'Intake Reporting') {
+                return await this.prisma.intakeReport.findMany({
+                    where: {
+                        fileId: id
+                    },
+                    include: {
+                        immediateNeeds: true,
+                        previousLivingSituations: true,
+                        citizenImmigrationStatuses: true,
+                        incomeTypes: true,
+                        veteranStatuses: true
+                    }
+                })
+            }
+        } catch (error) {
+            console.error('Error creating records:', error)
+            return { success: false, message: 'Failed to find a report' }
         }
-        return { message: 'Not Found' }
     }
 }
